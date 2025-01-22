2e1d15dcb0d2fdeb8915571db1b95d2fcf3a0c50
KasparKoorits <kaspar.koorits@voco.ee>
2025-01-22 13:02:51 +0200

    Get all Todos

--- routes/todos.js
+++ routes/todos.js
@@ -2,6 +2,8 @@ import express, { Router } from "express";
 import { TodoController } from "../controller/todos.js";
 
 const router = Router();
+
 router.post("/new-todo", (req, res) => TodoController.createTodo(req, res));
+router.get("/", (req, res) => TodoController.getTodos(req, res));
 
 export default router;
